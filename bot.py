a803d3d14d5599abd32d9f310a230c888d034fb8
SultanJumberto <kussainov.z@gmail.com>
2026-02-19 14:00:43 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,11 +8,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     token = os.environ.get("TELEGRAM_BOT_TOKEN")
     if not token:
-        token = "8318096413:AAFl58y0d_kHV4ep4co-8tX14hIqI9VVl5I"  # fallback для теста
+        token = "8318096413:AAFl58y0d_kHV4ep4co-8tX14hIqI9VVl5I"
     
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler("start", start))
-    application.run_polling(drop_pending_updates=True)
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
     main()
