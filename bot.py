fcb5ba0e95e90483ab1ec4f30f00c13bca04678c
SultanJumberto <kussainov.z@gmail.com>
2026-02-19 13:54:07 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ Привет! Я бот для постов в Инстаграм.")
 
 def main():
-    token = 8318096413:AAFl58y0d_kHV4ep4co-8tX14hIqI9VVl5I
+    token = "8318096413:AAFl58y0d_kHV4ep4co-8tX14hIqI9VVl5I"
     application = Application.builder().token(token).build()
     application.add_handler(CommandHandler("start", start))
     application.run_webhook(
